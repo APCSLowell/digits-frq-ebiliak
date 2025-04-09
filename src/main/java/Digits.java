e8b85a905389bea1b988efff93ba054cef7210fb
ebiliak <ilforoughi@s.sfusd.edu>
2025-04-09 09:21:46 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -6,12 +6,28 @@ public class Digits
 	private ArrayList<Integer> digitList;
 
 	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
+	{
+		digitlist = new ArrayList <integer>();
+		int digit;
+		int rem = num;
+		while(rem > 0){
+			digit = rem % 10;
+			rem = rem /10;
+			digitlis(0,digit);
+		}
+		
+	}
 	    
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
+		for(int i = 0; i < digitlist.size(); i++){
+			if(digitlist.get(i) >= digitlist.get(i+1)){
+				return false;
+			}
+			return true;
+		}
 		
 
 	}
