3b9505c117c05af53103a5d48e016725878b31d2
ebiliak <ilforoughi@s.sfusd.edu>
2025-04-10 21:57:26 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -6,17 +6,17 @@ public class Digits
 	private ArrayList<Integer> digitList;
 
 	public Digits(int num)
-	{
-		digitList = new ArrayList <Integer>();
-		int digit;
-		int rem = num;
-		while(rem > 0){
-			digit = rem % 10;
-			rem = rem /10;
-			digitList.add(0, digit);
-		}
-		
-	}
+{
+    digitList = new ArrayList<Integer>();
+    digitList.add(0, new Integer(num % 10));
+
+    int numRemaining = num / 10;
+    while(numRemaining > 0)
+    {
+        digitList.add(0, new Integer(numRemaining % 10));
+        numRemaining /= 10;
+    }
+}
 	    
 
 	public boolean isStrictlyIncreasing()
